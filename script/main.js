e7d545cc3035f4010f3a4281cd8de71d036be1e4
Sinan Bakim <sinanbakim@googlemail.colm>
2020-02-06 12:49:17 +0100

    [FIN]

--- script/main.js
+++ script/main.js
@@ -16,10 +16,12 @@ var curPlayer = 1;
 
 
 function resizeGrid() {
-    var l = $("#ttt-game")[0].offsetWidth / 3;
+    var l = $("#ttt-game")[0].offsetWidth;
     $("td")
-        .attr("width", l)
-        .attr("height", l);
+        .css("width", l / 3)
+        .css("height", l / 3);
+
+    $("#ttt-game").css("height", l);
 }
 
 function events() {
@@ -32,10 +34,12 @@ function events() {
 
     $("#ttt-start").on("click", function(){
         if(timer == 0 && clockTicking == false) {
+            showGrid();
+            resizeGrid();
             clockTicking = true;
             timer = 120;
             intervalHandler = window.setInterval(idle, 1000);
-            $("#ttt-game").removeClass("hide");
+            $("#ttt-game tbody").removeClass("hide");
             $("#ttt-start").html("Spiel pausieren");
             showClock();
         }
@@ -55,23 +59,10 @@ function events() {
         timer = 0;
         clockTicking = false;
         window.clearInterval(intervalHandler);
-        $("#ttt-start").html("Spiel starten")
+        $("#ttt-start").html("Spiel starten");
+        $("#ttt-game tbody").addClass("hide");
+        $("td").removeAttr("class");
         showClock();
-    });
-
-    $("td").on("click", function() {
-        if($(this).attr("class") == undefined) {
-            switch(curPlayer) {
-                case 1: 
-                    $(this).attr("class", "x");
-                    curPlayer = 2;
-                    break;
-                case 2:
-                    $(this).attr("class", "o");
-                    curPlayer = 1;
-                    break;
-            }
-        }
     });
 }
 
@@ -84,27 +75,59 @@ function getFieldPlayer(field) {
     }
 }
 
+function compareAll() {
+    if(arguments.length > 1) {
+        var i;
+        var last = arguments[0];
+        for (i = 1; i < arguments.length; i++) {
+            if(last == arguments[i]) {
+                last = arguments[i];
+            } else {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        console.log("Not enough arguments! Expected more than 1.")
+    }
+}
+
 function checkTriple() {
-    var fields = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var fields = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     var pMap = fields.map(getFieldPlayer);
-    if(pMap[0] == pMap[1] == pMap[2])
-        return pMap[0];
-    else if(pMap[3] == pMap[4] == pMap[5])
-        return pMap[3];
-    else if(pMap[6] == pMap[7] == pMap[8])
-        return pMap[6];
-    else if(pMap[0] == pMap[3] == pMap[6])
-        return pMap[0];
-    else if(pMap[1] == pMap[4] == pMap[7])
-        return pMap[1];
-    else if(pMap[2] == pMap[5] == pMap[8])
-        return pMap[2];
-    else if(pMap[0] == pMap[4] == pMap[8])
-        return pMap[0];
-    else if(pMap[2] == pMap[4] == pMap[6])
-        return pMap[2];
-    else
-        return 0;
+    if(compareAll(pMap[0], pMap[1], pMap[2], 1))
+        return 1;
+    if(compareAll(pMap[3], pMap[4], pMap[5], 1))
+        return 1;
+    if(compareAll(pMap[6], pMap[7], pMap[8], 1))
+        return 1;
+    if(compareAll(pMap[0], pMap[3], pMap[6], 1))
+        return 1;
+    if(compareAll(pMap[1], pMap[4], pMap[7], 1))
+        return 1;
+    if(compareAll(pMap[2], pMap[5], pMap[8], 1))
+        return 1;
+    if(compareAll(pMap[0], pMap[4], pMap[8], 1))
+        return 1;
+    if(compareAll(pMap[2], pMap[4], pMap[6], 1))
+        return 1;
+    if(compareAll(pMap[0], pMap[1], pMap[2], 2))
+        return 2;
+    if(compareAll(pMap[3], pMap[4], pMap[5], 2))
+        return 2;
+    if(compareAll(pMap[6], pMap[7], pMap[8], 2))
+        return 2;
+    if(compareAll(pMap[0], pMap[3], pMap[6], 2))
+        return 2;
+    if(compareAll(pMap[1], pMap[4], pMap[7], 2))
+        return 2;
+    if(compareAll(pMap[2], pMap[5], pMap[8], 2))
+        return 2;
+    if(compareAll(pMap[0], pMap[4], pMap[8], 2))
+        return 2;
+    if(compareAll(pMap[2], pMap[4], pMap[6], 2))
+        return 2;
+    return 0;
 }
 
 function showGUI() {
@@ -117,14 +140,15 @@ function showGUI() {
 }
 
 function showClock() {
-    $("#ttt-info").html(timer);
+    $("#ttt-info p").html(timer);
 }
 
-function selectPrompt(i) {
+function showPrompt(i) {
     switch(i) {
-        case 0: $("#ttt-info").html("Spiel vorbei!"); break;
-        case 1: $("#ttt-info").html("Spieler 1 gewinnt!"); break;
-        case 2: $("#ttt-info").html("Spieler 2 gewinnt!"); break;
+        case 0: $("#ttt-game").html('<span class="center"><p>Spiel vorbei!</p></span>'); break;
+        case 1: $("#ttt-game").html('<span class="center"><p>Spieler X gewinnt!</p></span>'); break;
+        case 2: $("#ttt-game").html('<span class="center"><p>Spieler O gewinnt!</p></span>'); break;
+        default: $("#ttt-game").html('<span class="center"><p></p></span>'); break;
     }
     
 }
@@ -142,22 +166,43 @@ function countdown() {
 
 function idle() {
     countdown();
-    if(checkTriple() == 0 && timer == 0) {
-        window.clearInterval(intervalHandler);
-        selectPrompt(0);
-    }
-    if(checkTriple() == 1) {
-        window.clearInterval(intervalHandler);
-        selectPrompt(1);
-    }
-    if(checkTriple() == 2) {
-        window.clearInterval(intervalHandler);
-        selectPrompt(2);
-    }
+
+}
+
+function showGrid() {
+    $("#ttt-game").html('<table><tbody><tr><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td></tr></tbody></table>');
+    $("td").on("click", function() {
+        if($(this).attr("class") == undefined) {
+            switch(curPlayer) {
+                case 1: 
+                    $(this).attr("class", "x");
+                    curPlayer = 2;
+                    break;
+                case 2:
+                    $(this).attr("class", "o");
+                    curPlayer = 1;
+                    break;
+            }
+        }
+        var ttt = checkTriple();
+        if(ttt == 0 && timer == 0) {
+            window.clearInterval(intervalHandler);
+            showPrompt(0);
+        }
+        if(ttt == 1) {
+            window.clearInterval(intervalHandler);
+            showPrompt(1);
+        }
+        if(ttt == 2) {
+            window.clearInterval(intervalHandler);
+            showPrompt(2);
+        }
+    });
 }
 
 function init() {
     showGUI();
+    showPrompt();
     resizeGrid();
     events();
 }
@@ -165,6 +210,3 @@ function init() {
 function main() {
     init();
 }
-
-
-
